936c1b9fc405763aac1aa711faf1b8fbdccd4c84
Nacir Chahine <120245923+NacirChahine@users.noreply.github.com>
2024-09-30 13:21:35 +0300

    set display caption and icon

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import pygame
 from game_assets import load_assets
 from screens import show_welcome_screen
 from game_logic import game_loop
+from utils import resource_path
 
 # Initialize Pygame and assets
 pygame.init()
 pygame.mixer.init()
+
+# Set the window title
+pygame.display.set_caption("Space Travel | Naro Chan Dev")
+
+# Load and set the window icon
+icon = pygame.image.load(resource_path('space_ship.png'))
+pygame.display.set_icon(icon)
 
 
 def main():
